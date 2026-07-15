cba42e0e3fa36bc70c96fd1dc55531da00b7a789
ayoussfi605-crypto <ayoussfi605@gmail.com>
2026-07-15 01:09:40 +0100

    add the logout function

---
+++ backend/handlers/logoutHandler.go
@@ -0,0 +1,36 @@
+ackage handlers
+ 
+import (
+	db "forum/database"
+	"forum/helpers"
+	"net/http"
+)
+ 
+func LogoutHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		helpers.SendJSON(w, http.StatusMethodNotAllowed, "Method not allowed")
+		return
+	}
+ 
+	cookie, err := r.Cookie("Form_Token")
+	if err != nil {
+		helpers.SendJSON(w, http.StatusUnauthorized, "No session found")
+		return
+	}
+ 
+	_, err = db.DB.Exec("DELETE FROM sessions WHERE token = ?", cookie.Value)
+	if err != nil {
+		helpers.SendJSON(w, http.StatusInternalServerError, "Internal Server Error")
+		return
+	}
+ 
+	http.SetCookie(w, &http.Cookie{
+		Name:   "Form_Token",
+		Value:  "",
+		Path:   "/",
+		MaxAge: -1,
+	})
+ 
+	helpers.SendJSON(w, http.StatusOK, "Logout successful")
+}
+ 
