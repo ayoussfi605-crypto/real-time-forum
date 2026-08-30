717672bf97097ae924eca19f4cfb154ec1a20ed0
aelyoussef <ayoussfi605@gmail.com>
2026-08-30 14:03:42 +0100

    fix the age error

--- backend/helpers/validateRegisterInput.go
+++ backend/helpers/validateRegisterInput.go
@@ -11,17 +11,23 @@ func ValidateRegisterInput(nickname string, firstName string, lastName string, a
 		SendJSON(w, http.StatusBadRequest, "Invalid gender. Please select 'Male' or 'Female'")
 		return false
 	}
-
-	if nickname == "" || firstName == "" || lastName == "" ||
-		age <= 9 || age > 150 || email == "" || password == "" {
+	
+	if nickname == "" || firstName == "" ||
+	lastName == "" || email == "" || password == "" {
 		SendJSON(w, http.StatusBadRequest, "Please fill all the fields")
 		return false
 	}
-
+	
 	if len(nickname) < 3 || len(nickname) > 50 || len(firstName) < 3 || len(firstName) > 50 || len(lastName) < 3 || len(lastName) > 50 {
 		SendJSON(w, http.StatusBadRequest, "Nickname, first name, and last name must be between 3 and 50 characters long")
 		return false
 	}
+
+	if age <= 9 || age > 150 {
+			SendJSON(w, http.StatusBadRequest, "age must be between 9 and 150")
+			return false
+	}
+
 	_, err := mail.ParseAddress(email)
 	if err != nil {
 		SendJSON(w, http.StatusBadRequest, "Invalid email address")
